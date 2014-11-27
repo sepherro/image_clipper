eb5c06c42fe89ae823b2642ca58d67ef3b04db93
Marek <marek.kraft@gmail.com>
2014-11-27 22:28:27 +0100

    moved to opencv, added some functionality

--- main.py
+++ main.py
@@ -1,98 +1,81 @@
-import tkinter
-from PIL import Image, ImageTk
+import cv2
+import numpy as np
 import glob
 import winsound
+import time
 
 listoffiles=[]
+topleft_coords = [0, 0]
 
-snap_xsize = 15
-snap_ysize = 15
+# predefined sizes of cutout window you can cycle through [width, height]
+cutout_sizes = [ [64, 128], [77, 154] ]
+cutout_size_idx = 0
 
-image_idx=0
-cutout_idx=0
+image_idx = 0
+cutout_idx = 0
 
-for file in glob.glob("*.png"):
-    print(file)
-    listoffiles.append(file)
+# build a list of sample images
+for image_file in glob.glob("pos/*.png"):
+    print(image_file)
+    listoffiles.append(image_file)
 
-
-x = 0
-y = 0
-
-#Tkinter stuff window creation
-window = tkinter.Tk()
-#open image
-image = Image.open(listoffiles[image_idx])
-#create canvas of given size
-canvas = tkinter.Canvas(window, width=image.size[0], height=image.size[1])
-canvas.pack()
-#convert to a class that can be displayed on the canvas; display the image
-image_tk = ImageTk.PhotoImage(image)
-canvas.create_image(image.size[0]//2, image.size[1]//2, image=image_tk)
-canvas.configure(cursor="crosshair")
-
-def lmb_callback(event):
-    print("clicked at: ", event.x, event.y)
-    global x                    # need to find a more 'pythoney' way to do it
-    global y
-    x = event.x
-    y = event.y
-    canvas.create_image(image.size[0]//2, image.size[1]//2, image=image_tk)
-    canvas.create_line(x-snap_xsize, y-snap_ysize, x+snap_xsize, y-snap_ysize, x+snap_xsize, y+snap_ysize,
-                       x-snap_xsize, y+snap_ysize, x-snap_xsize, y-snap_ysize)
-    return x, y
-
-
-def rmb_callback(event):
-    global cutout_idx
-    print("rmb pressed! x=", x, "y=", y)
-    box = (x-snap_xsize, y-snap_ysize, x+snap_xsize, y+snap_ysize)
-    region = image.crop(box)
-    path = str(print("samples/cutout_%05d.png" % cutout_idx))
-    region.save("samples/cutout_%05d.png" % cutout_idx, "PNG")
-    winsound.Beep(1000, 50)
+# count the number of samples collected so far
+for sample_file in glob.glob("samples/*.*"):
     cutout_idx += 1
 
+# read the first image from the list
+image = cv2.imread(listoffiles[image_idx])
 
-def key_callback(event):
-    global snap_xsize           # need to find a more 'pythoney' way to do it
-    global snap_ysize
-    global image_idx
-    global image
-    global image_tk
-    if event.char == 'x' and (image_idx < len(listoffiles)-1):
+# callback function for mouse events
+def callback_function(event,x,y,flags,param):
+    global topleft_coords, cutout_idx
+    if event == cv2.EVENT_LBUTTONDOWN:
+        image_copy = np.copy(image)
+        cv2.rectangle(image_copy, (x, y), (x + cutout_sizes[cutout_size_idx][0], y + cutout_sizes[cutout_size_idx][1]), (0, 255, 0), 1)
+        cv2.imshow("image", image_copy)
+        topleft_coords[0] = x
+        topleft_coords[1] = y
+    if event == cv2.EVENT_RBUTTONDOWN:
+        winsound.Beep(440, 200)
+        time.sleep(0.2)
+        region = image[topleft_coords[1] : topleft_coords[1] + cutout_sizes[cutout_size_idx][1], topleft_coords[0] : topleft_coords[0] + cutout_sizes[cutout_size_idx][0]]
+        path = "samples/cutout_%05d.png" % cutout_idx
+        cv2.imwrite(path, region)
+        cutout_idx += 1
+        print path + " saved!"
+
+# create display window, show the initial image
+cv2.namedWindow('image')
+cv2.imshow("image", image)
+
+# bind the mouse events to callback function
+cv2.setMouseCallback('image', callback_function)
+
+
+while(True):
+    key_pressed = cv2.waitKey(1) & 0xFF
+    # load and show next image from the list
+    if key_pressed == ord('x') and (image_idx < len(listoffiles)-1):
         image_idx += 1
-        image = Image.open(listoffiles[image_idx])
-        image_tk = ImageTk.PhotoImage(image)
-        canvas.create_image(image.size[0]//2, image.size[1]//2, image=image_tk)
-        print("displaying image", image_idx+1, " out of ", len(listoffiles)-1)
-    if event.char == 'z' and (image_idx > 0):
+        image = cv2.imread(listoffiles[image_idx])
+        cv2.imshow("image", image)
+        print "displaying image", image_idx+1, " out of ", len(listoffiles)-1
+    # load and show previous image from the list
+    if key_pressed == ord('z') and (image_idx > 0):
         image_idx -= 1
-        image = Image.open(listoffiles[image_idx])
-        image_tk = ImageTk.PhotoImage(image)
-        canvas.create_image(image.size[0]//2, image.size[1]//2, image=image_tk)
-        print("displaying image", image_idx+1, " out of ", len(listoffiles)-1)
-    if event.char == 's' and snap_xsize < 30:
-        snap_xsize += 5
-        snap_ysize += 5
-        print("window size is now ", snap_xsize, " by ", snap_ysize, " pixels")
-    if event.char == 'a' and snap_xsize > 5:
-        snap_xsize -= 5
-        snap_ysize -= 5
-        print("window size is now ", snap_xsize, " by ", snap_ysize, " pixels")
+        image = cv2.imread(listoffiles[image_idx])
+        cv2.imshow("image", image)
+        print "displaying image", image_idx+1, " out of ", len(listoffiles)-1
+    # increase image sample size
+    if key_pressed == ord('s') and cutout_size_idx < len(cutout_sizes) - 1:
+        cutout_size_idx += 1
+        print "cutout size is " + str(cutout_sizes[cutout_size_idx])
+    # decrease image sample size
+    if key_pressed == ord('a') and cutout_size_idx > 0:
+        cutout_size_idx -= 1
+        print "cutout size is " + str(cutout_sizes[cutout_size_idx])
+    # exit when ESC hit
+    elif key_pressed == 27:
+        break
 
-
-#the line below is necessary for the keypress callback to work
-canvas.focus_set()
-
-canvas.bind("<Button-1>", lmb_callback)
-canvas.bind("<Button-3>", rmb_callback)
-canvas.bind("<Key>", key_callback)
-
-
-
-
-print(listoffiles[1])
-
-
-tkinter.mainloop()
+cv2.destroyAllWindows()
